a40e7fa070a259872a91b7e8ff27a815a6060041
skroflin <sven.kroflin@gmail.com>
2025-02-10 18:30:33 +0100

    beginning

---
+++ lesson-01/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // The `main` function runs every executable Rust program
+    println!("Hello, world!");
+    // The body of the `main` function holds this code
+    // `println!` calls a Rust macro --> `println` calls a Rust function
+}
+
+// To run a Rust program, you must compile it first using the `rustc` command and passing the file name
+// After compiling, you run the executable file `file_name.exe` (the name of the executable depends on function naming)
